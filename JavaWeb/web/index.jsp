dd5195ac7d9dc0d005fdf3cc9bde0c22f4cf4968
“LiuHonglve” <lhlxyu@163.com>
2022-03-06 17:39:50 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lhlxy
+  Date: 2022/3/4
+  Time: 23:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>尚硅谷官网。。。柳</title>
+  </head>
+  <body>
+    <h1 style="color: orange">hello,idea</h1>
+  </body>
+</html>
---
+++ src/com/lhl/java/TemplatesTest.java
@@ -0,0 +1,36 @@
+package com.lhl.java;
+
+/**
+ * @author liuhonglue
+ * @date 2022-03-04 23:05
+ *
+ * IDEA中代码模板所处的位置：live templates / postfix completion
+ */
+public class TemplatesTest {
+
+    //模板一：psvm
+    public static void main(String[] args) {
+
+    //模板二：
+        System.out.println("Hello");
+        System.out.println("TemplatesTest.main");
+        System.out.println("args = " + args);
+
+
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num1);
+
+
+        for (String arg : args) {
+            
+        }
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            
+        }
+
+
+    }
+
+}
